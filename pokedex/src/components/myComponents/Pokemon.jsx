0a09a95075c8224d4814ff48e74731fd309b3e28
Cristian martini <cristianmartini.sp@gmail.com>
2024-08-21 20:49:20 -0300

    respondividade navbar

--- pokedex/src/components/myComponents/Pokemon.jsx
+++ pokedex/src/components/myComponents/Pokemon.jsx
@@ -8,7 +8,7 @@ const Pokemon = () => {
   useEffect(() => {
     const fetchData = async () => {
       const promises = [];
-      for (let i = 1; i <= 20; i++) {
+      for (let i = 1; i <= 1000; i++) {
         promises.push(axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`));
       }
       const responses = await Promise.all(promises);
--- pokedex/src/components/nav/NavBar.jsx
+++ pokedex/src/components/nav/NavBar.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo"><img src="https://cdn.icon-icons.com/icons2/851/PNG/512/Pokedex_tool_icon-icons.com_67529.png" alt="" height={"40px"} width={"40px"} />
-       
+      <div className="navbar-logo">
+        <img src="https://cdn.icon-icons.com/icons2/851/PNG/512/Pokedex_tool_icon-icons.com_67529.png" alt="Pokedex Logo" height="40px" width="40px" />
       </div>
-      <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">Mais Pokemons</a></li>
-       
-      </ul>
+
+      <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Mais Pokémons</a></li>
+        </ul>
+      </div>
+
       <div className="navbar-cta">
         <a href="#signup" className="btn-primary">Login</a>
+      </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
